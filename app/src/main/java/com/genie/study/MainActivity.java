728393eb997fa5d37e3b204ddec97cf14993d3e2
nth1991 <huongnt349353@gmail.com>
2017-07-08 19:29:59 +0700

    First Commit

---
+++ app/src/main/java/com/genie/study/MainActivity.java
@@ -0,0 +1,13 @@
+package com.genie.study;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
