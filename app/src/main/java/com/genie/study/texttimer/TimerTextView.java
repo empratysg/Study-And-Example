fa82f0c6a7654bb1d43cbdf03d3d00bfbf2ba317
nth1991 <huongnt349353@gmail.com>
2017-07-09 14:56:33 +0700

    update timer text view

--- app/src/main/java/com/genie/study/texttimer/TimerTextView.java
+++ app/src/main/java/com/genie/study/texttimer/TimerTextView.java
@@ -11,13 +11,15 @@ import android.os.SystemClock;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 
 /**
  * Created by Genie on 7/8/2017.
+ * Text view count time with input
  */
 
 public class TimerTextView extends AppCompatTextView {
+    private static final String TAG = "TimerTextView";
+    private static final int ONE_SECOND_MILLS = 1000;
     private static final int MINUTE_SECOND = 60;
     private static final int HOUR_MINUTE = 60;
     private static final int HOUR_SECOND = 3600;
@@ -44,7 +46,7 @@ public class TimerTextView extends AppCompatTextView {
         public void run() {
             onTimeChanged();
             long now = SystemClock.uptimeMillis();
-            long next = now + (1000 - now % 1000);
+            long next = now + ONE_SECOND_MILLS;
 
             Handler handler = getHandler();
             handler.removeCallbacks(mTicker);
@@ -55,49 +57,60 @@ public class TimerTextView extends AppCompatTextView {
     @SuppressLint("DefaultLocale")
     private void onTimeChanged() {
         long time = SystemClock.uptimeMillis();
+        int remainTime = remainTimeToSeconds();
         int secondPast;
-        int minutePast = 0;
-        int hourPast = 0;
         if (lastTimeChange == 0) {
-            lastTimeChange = SystemClock.uptimeMillis();
             secondPast = 1;
         } else {
             long offset = time - lastTimeChange;
             offset = offset - (offset % 1000);
             secondPast = (int) (offset / 1000);
-            lastTimeChange = time;
         }
-
-        if (seconds >= secondPast) {
-            seconds -= secondPast;
+        lastTimeChange = time;
+        if (remainTime >= secondPast) {
+            remainTime -= secondPast;
+            hours = hourFromTime(remainTime);
+            minutes = minuteFromTime(remainTime);
+            seconds = secondFromTime(remainTime);
         } else {
-            seconds = MINUTE_SECOND - (secondPast % MINUTE_SECOND);
-            if (secondPast >= MINUTE_SECOND)
-                minutePast = (secondPast - (secondPast % MINUTE_SECOND)) / MINUTE_SECOND;
-            else
-                minutePast = 1;
-        }
-
-        if (minutes >= minutePast) {
-            minutes -= minutePast;
-        } else {
-            minutes = HOUR_MINUTE - (minutePast % HOUR_MINUTE);
-            if (minutePast >= HOUR_MINUTE)
-                hourPast = (minutePast - (minutePast % HOUR_MINUTE)) / HOUR_MINUTE;
-            else
-                hourPast = 1;
-        }
-        if (hours >= hourPast) {
-            hours -= hourPast;
-            setText(String.format(FORMAT_TIME_OUTPUT_DEFAULT, hours, minutes, seconds));
-        } else {
+            sourceTime = null;
             hours = 0;
-            setText(END_OF_TIMER);
+            minutes = 0;
+            seconds = 0;
+            Handler handler = getHandler();
+            if (handler != null)
+                handler.removeCallbacks(mTicker);
         }
+        setText(String.format(FORMAT_TIME_OUTPUT_DEFAULT, hours, minutes, seconds));
     }
 
+    private int remainTimeToSeconds() {
+        return (hours * HOUR_SECOND) + (minutes * MINUTE_SECOND) + seconds;
+    }
+
+    private int hourFromTime(int seconds) {
+        int totalHourInSecond = seconds - (seconds % HOUR_SECOND);
+        return totalHourInSecond / HOUR_SECOND;
+    }
+
+    private int minuteFromTime(int seconds) {
+        int totalMinuteInSecond = seconds - (seconds % MINUTE_SECOND);
+        int totalMinute = totalMinuteInSecond / MINUTE_SECOND;
+        return totalMinute % HOUR_MINUTE;
+    }
+
+    private int secondFromTime(int seconds) {
+        return seconds % MINUTE_SECOND;
+    }
+
+    /**
+     * @param source     String Source Time
+     * @param resetTimer if true timer will always change with source input
+     *                   otherwise check if source time is change or not for reset timer
+     */
     public void setSourceTime(String source, boolean resetTimer) {
-        if (resetTimer || sourceTime == null || !sourceTime.equals(source)) {
+        if (TextUtils.isEmpty(source)) return;
+        if (resetTimer || TextUtils.isEmpty(sourceTime) || !sourceTime.equals(source)) {
             sourceTime = source;
             getHourMinuteSecondFromSource();
         }
@@ -109,12 +122,13 @@ public class TimerTextView extends AppCompatTextView {
     }
 
     public void setSourceTime(String source) {
-        setSourceTime(source, false);
+        setSourceTime(source, true);
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+        if (TextUtils.isEmpty(sourceTime)) return;
         Handler handler = getHandler();
         handler.removeCallbacks(mTicker);
         handler.postDelayed(mTicker, 1000);
@@ -134,7 +148,11 @@ public class TimerTextView extends AppCompatTextView {
     private void getHourMinuteSecondFromSource() {
         hours = Integer.parseInt(sourceTime.substring(0, 2));
         minutes = Integer.parseInt(sourceTime.substring(2, 4));
+        if (minutes >= HOUR_MINUTE)
+            minutes = HOUR_MINUTE - 1;
         seconds = Integer.parseInt(sourceTime.substring(4));
+        if (seconds >= MINUTE_SECOND)
+            seconds = MINUTE_SECOND - 1;
         lastTimeChange = 0;
         setText(String.format(FORMAT_TIME_OUTPUT_DEFAULT, hours, minutes, seconds));
     }
@@ -164,15 +182,19 @@ public class TimerTextView extends AppCompatTextView {
         this.minutes = savedState.minutes;
         this.seconds = savedState.seconds;
         this.lastTimeChange = savedState.lastTimeChange;
-        setText(String.format(FORMAT_TIME_OUTPUT_DEFAULT, hours, minutes, seconds));
+        onTimeChanged();
+        if (TextUtils.isEmpty(sourceTime)) return;
+        Handler handler = getHandler();
+        handler.removeCallbacks(mTicker);
+        handler.postDelayed(mTicker, 1000);
     }
 
-    static class SavedState extends BaseSavedState {
+    private static class SavedState extends BaseSavedState {
         private String sourceTime;
         private int hours, minutes, seconds;
         private long lastTimeChange;
 
-        public SavedState(Parcel source) {
+        SavedState(Parcel source) {
             super(source);
             this.sourceTime = source.readString();
             this.hours = source.readInt();
@@ -182,7 +204,7 @@ public class TimerTextView extends AppCompatTextView {
         }
 
         @TargetApi(Build.VERSION_CODES.N)
-        public SavedState(Parcel source, ClassLoader loader) {
+        SavedState(Parcel source, ClassLoader loader) {
             super(source, loader);
             this.sourceTime = source.readString();
             this.hours = source.readInt();
@@ -191,7 +213,7 @@ public class TimerTextView extends AppCompatTextView {
             this.lastTimeChange = source.readLong();
         }
 
-        public SavedState(Parcelable superState) {
+        SavedState(Parcelable superState) {
             super(superState);
         }
 
--- app/src/main/java/com/genie/study/texttimer/TimerTextViewFragment.java
+++ app/src/main/java/com/genie/study/texttimer/TimerTextViewFragment.java
@@ -3,10 +3,12 @@ package com.genie.study.texttimer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.genie.study.R;
 
@@ -15,7 +17,23 @@ import com.genie.study.R;
  */
 
 public class TimerTextViewFragment extends Fragment implements View.OnClickListener {
+    private static final String EXTRA_INPUT_SOURCE = "extra_input_source";
     private TimerTextView timerTextView;
+    private EditText input;
+    private String inputSource;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null)
+            inputSource = savedInstanceState.getString(EXTRA_INPUT_SOURCE);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(EXTRA_INPUT_SOURCE, inputSource);
+    }
 
     @Nullable
     @Override
@@ -26,7 +44,10 @@ public class TimerTextViewFragment extends Fragment implements View.OnClickListe
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         timerTextView = view.findViewById(R.id.timer);
+        input = view.findViewById(R.id.input_time);
+
         view.findViewById(R.id.change_fragment).setOnClickListener(this);
+        view.findViewById(R.id.set_input).setOnClickListener(this);
     }
 
     @Override
@@ -37,12 +58,17 @@ public class TimerTextViewFragment extends Fragment implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        getFragmentManager().beginTransaction().replace(R.id.frame, new AnotherFragment()).addToBackStack(null).commit();
-    }
-
-    @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
-        timerTextView.setSourceTime("720000");
+        switch (view.getId()) {
+            case R.id.change_fragment:
+                getFragmentManager().beginTransaction().replace(R.id.frame, new AnotherFragment()).addToBackStack(null).commit();
+                break;
+            case R.id.set_input:
+                String source = input.getText().toString();
+                if (!TextUtils.isEmpty(source.trim())) {
+                    inputSource = source;
+                    timerTextView.setSourceTime(source);
+                }
+                break;
+        }
     }
 }
